24ea98ab32fdb96d0e062f11956e5afd3bf576df
Joshi Binaya <bjoshi8@LAMU02D12H0MD6R.uhc.com>
2021-12-26 07:25:33 -0600

    dynamic_version

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   first_name = "forum"
   last_name  = "punte"
-   
+  sg         = [{ in = 80, out = 90, desc = "1st Rule", cidr_blocks = ["192.168.0.0/24"] }, { in = 90, out = 92, desc = "2nd Rule", cidr_blocks = ["192.168.4.0/24"] }]
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
-	type=string
- 
-	default = "us-east-1"
+  type = string
+
+  default = "us-east-1"
 }
